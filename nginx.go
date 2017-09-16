4fbe2257162501eab5bd855c833989475c751c04
Ubuntu <ubuntu@ip-172-31-22-2.ca-central-1.compute.internal>
2017-09-16 10:54:08 +0000

    Update git ignore.  Add nginx.go, which includes function to update conf with base dynamic setup (web for now)

---
+++ nginx.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"io/ioutil"
+	//"fmt"
+	"strings"
+	"log"
+)
+
+func main() {
+	//Open nginx config
+	conf, err := ioutil.ReadFile("/etc/nginx/sites-available/default")
+	if (err != nil) {
+		panic(err)
+	}
+
+	//Split config into lines for easier handling
+	confLines := strings.Split(string(conf), "\n")
+
+	//Locate the last bracket (the closing server bracket)
+	lastBracket := 0
+	for i, line := range confLines {
+		if strings.Contains(line, "}") {
+			lastBracket = i
+		}
+	}
+	//Panic if it isn't found
+	if (lastBracket == 0) {
+		panic("Closing Bracket not found!")
+	}
+
+	var newConf []string
+
+	newConf = append(newConf, "")
+	newConf = append(newConf, "\t# START <user>/<repo>")
+
+	locString := "\tlocation /repo/user/repo"
+
+	//TODO add compat to check if web app or not (assuming true for now)
+	isWeb := true
+	if (isWeb) {locString += "/"}
+
+	locString += " {"
+
+	newConf = append(newConf, locString)
+
+	if (isWeb) {newConf = append(newConf, "\t\talias /var/app/deploy/user/repo;")}
+
+	newConf = append(newConf, "\t}")
+	newConf = append(newConf, "\t# END <user>/<repo>")
+
+	confLines = append(confLines[:lastBracket], append(newConf, confLines[lastBracket:]...)...)
+
+	confOutput := strings.Join(confLines, "\n")
+	err = ioutil.WriteFile("/etc/nginx/sites-available/default",  []byte(confOutput), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
