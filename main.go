6557b3a91dea219439e490b38595cff55607917d
gprattico <gprattico95@gmail.com>
2017-09-16 05:20:21 -0400

    add file serve capability

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
-import ("fmt"
-       "html"
+import (//"fmt"
+       //"html"
        "log"
        "net/http"
        )
 
 func main() {
-	fmt.Printf("hello, world\n")
+	
+	fs := http.FileServer(http.Dir("dashboard"))
+  	http.Handle("/dashboard/", http.StripPrefix("/dashboard", fs))
 
-	 http.HandleFunc("/Dashboard/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
-
-    log.Fatal(http.ListenAndServe(":8080", nil))
+  	log.Println("Listening...")
+  	http.ListenAndServe(":8080", nil)
+    
 }
 
 /**
