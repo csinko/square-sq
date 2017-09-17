84d8fa0202005d9222a5b0a051300279dba7efa0
Cameron Sinko <csinko@gmail.com>
2017-09-16 22:05:56 -0400

    Add Database

--- main.go
+++ main.go
@@ -9,25 +9,64 @@ import (
 	"net/http"
 	//"time"
 	//"strings"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-//function to collect post requests
-func parsePost(w http.ResponseWriter, r *http.Request) {
+func getRepos(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	//Check if request is a POST request
-	if r.Method == "POST" {
+		session := s.Copy()
+		defer session.Close()
 
-		//Read the body of the request.  If thers an error, return an error.
-		body, err := ioutil.ReadAll(r.Body)
+		c := session.DB("hackthenorth17").C("repos")
+
+		var repos []Webhook
+		err := c.Find(bson.M{}).All(&repos)
+
 		if err != nil {
-			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			http.Error(w, "Error failed to get repos", http.StatusInternalServerError)
+			fmt.Println("Failed to get repos: ", err)
 			return
 		}
 
-		//Print out body for debug
-		fmt.Println(string(body))
+		respBody, err := json.MarshalIndent(repos, "", "  ")
 
-		//Parse body JSON into object
+		if err != nil {
+			fmt.Println("Failed to convert")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write(respBody)
+
+	}
+}
+
+//function to collect post requests
+func parsePost(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		session := s.Copy()
+		defer session.Close()
+
+		c := session.DB("hackthenorth17").C("repos")
+
+		//Check if request is a POST request
+		if r.Method == "POST" {
+
+			//Read the body of the request.  If thers an error, return an error.
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Error reading request body", http.StatusInternalServerError)
+				return
+			}
+
+			//Print out body for debug
+			fmt.Println(string(body))
+
+			//Parse body JSON into object
 
 			// Unmarshal
 			var msg Webhook
@@ -37,21 +76,55 @@ func parsePost(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
+			var curRepo Webhook
+			err = c.Find(bson.M{"Repository.ID": msg.Repository.ID}).One(&curRepo)
+
+			//If the repo was not found (its new)
+			if err != nil {
+				if err = c.Insert(msg); err != nil {
+					panic(err)
+				}
+				fmt.Println("Added to Database")
+				fmt.Fprint(w, "Success")
+				return
+			}
+
+			curRepo.Commits = append(curRepo.Commits, msg.Commits...)
+
+			err = c.Update(bson.M{"Repository.ID": curRepo.Repository.ID}, &curRepo)
+			if err != nil {
+				fmt.Println("Failed to Update")
+				fmt.Fprint(w, "FAIL")
+			}
+
 			jsonOut, _ := json.Marshal(msg)
 			fmt.Println(string(jsonOut))
 
 			fmt.Println("Done")
 
-		//Send success as a response
-		fmt.Fprint(w, "Success")
+			//Send success as a response
 
-	} else {
-		//Return an error if not POST
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		} else {
+			//Return an error if not POST
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
 	}
 }
 
 func main() {
+
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:    []string{"ds139844.mlab.com:39844"},
+		Username: "htn2017",
+		Password: "htn2017",
+		Database: "hackthenorth17",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+	fmt.Printf("Connected to %v!\n", session.LiveServers())
 
 	//accept front end files
 	fs := http.FileServer(http.Dir("dashboard"))
@@ -59,7 +132,8 @@ func main() {
 
 	//create mux to identify post type
 	mux := http.NewServeMux()
-	mux.HandleFunc("/push", parsePost)
+	mux.HandleFunc("/push", parsePost(session))
+	mux.HandleFunc("/api/repos", getRepos(session))
 
 	log.Println("Listening...")
 	http.ListenAndServe(":8080", mux)
