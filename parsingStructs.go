e1ee43c6265325372f58e7b60ede79224df1a93b
Cameron Sinko <csinko@gmail.com>
2017-09-17 00:45:07 +0000

    Fix struct

--- parsingStructs.go
+++ parsingStructs.go
@@ -5,38 +5,23 @@ import (
 )
 
 
-type Webhook struct{
-
-	Commits []struct{
-
-		commitID string `json:"id"`
-		commitMessage string `json:"message"`
-		commitTimestamp time.Time `json:"timestamp"`
-
-		Author struct {
-
-			authorEmail    string `json:"email"`
-			authorName     string `json:"name"`
-			authorUsername string `json:"username"`
-
-		} `json:"author"`
-
-
-	} `json:"commits"`
-
-	Repository struct{
-
-		repoName  string `json:"name"`
-		repoID int64 `json:"id"`
-
-		Owner struct {
-
-			Name string `json:"Name"`
-
-		} `json:"owner"`
-
-
-
-	} `json:"repository"`
-
+type Webhook struct {
+        Commits []struct {
+                ID     string   `json:"id"`
+                Author struct {
+                        Email    string `json:"email"`
+                        Name     string `json:"name"`
+                        Username string `json:"username"`
+                } `json:"author"`
+                Message   string        `json:"message"`
+                Timestamp time.Time     `json:"timestamp"`
+        } `json:"commits"`
+        Repository struct {
+                ID               int64       `json:"id"`
+                Name             string      `json:"name"`
+                Owner            struct {
+                        Name              string `json:"name"`
+                } `json:"owner"`
+        } `json:"repository"`
 }
+
